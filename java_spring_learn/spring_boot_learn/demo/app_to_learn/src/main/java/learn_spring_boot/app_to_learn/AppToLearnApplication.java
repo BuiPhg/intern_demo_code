e867550a7bbc83fe75527419c90c7f2830654d58
Phuong Bui <phuong.bt@teko.vn>
2022-07-25 07:25:08 +0700

    spring boot demo code

---
+++ java_spring_learn/spring_boot_learn/demo/app_to_learn/src/main/java/learn_spring_boot/app_to_learn/AppToLearnApplication.java
@@ -0,0 +1,13 @@
+package learn_spring_boot.app_to_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppToLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppToLearnApplication.class, args);
+	}
+
+}
---
+++ java_spring_learn/spring_boot_learn/demo/src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        // ApplicationContext chính là container, chứa toàn bộ các Bean
+        ApplicationContext context = SpringApplication.run(App.class, args);
+
+        // Khi chạy xong, lúc này context sẽ chứa các Bean có đánh
+        // dấu @Component.
+
+        // Lấy Bean ra bằng cách
+        Outfit outfit = context.getBean(Outfit.class);
+
+        // In ra để xem thử nó là gì
+        System.out.println("Instance: " + outfit);
+        // xài hàm wear()
+        outfit.wear();
+    }
+}
